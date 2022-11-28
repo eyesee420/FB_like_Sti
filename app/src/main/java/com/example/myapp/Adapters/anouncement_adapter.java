32289507700f299f3355828d9769ad6ebcaa27bc
sselfles <95219729+sselfles@users.noreply.github.com>
2022-11-28 15:03:42 +0800

    AnnouncementUpdate

--- app/src/main/java/com/example/myapp/Adapters/anouncement_adapter.java
+++ app/src/main/java/com/example/myapp/Adapters/anouncement_adapter.java
@@ -18,32 +18,24 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 public class anouncement_adapter extends FirestoreRecyclerAdapter<Anouncements, anouncement_adapter.holder> {
-
     public anouncement_adapter(@NonNull FirestoreRecyclerOptions<Anouncements> options) {
         super(options);
     }
-
     @Override
     protected void onBindViewHolder(@NonNull anouncement_adapter.holder holder, int i, @NonNull Anouncements model) {
         holder.bind(model);
     }
-
     @NonNull
     @Override
     public anouncement_adapter.holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_anouncement, parent, false);
         return new anouncement_adapter.holder(view);
     }
-
     public class holder extends RecyclerView.ViewHolder {
-
         Context context;
         ImageView image_view;
         TextView what , when ,how, date_time;
-
-
         public holder(@NonNull View itemView) {
             super(itemView);
             context =itemView.getContext();
@@ -54,19 +46,14 @@ public class anouncement_adapter extends FirestoreRecyclerAdapter<Anouncements, 
             date_time = itemView.findViewById(R.id.date_time);
 
         }
-
         public void bind(Anouncements model) {
-
             Glide.with(context.getApplicationContext())
                     .load(model.getImage_uri())
                     .into(image_view);
-
-            what.setText("WHAT ? " +model.getWhat());
-            when.setText("HOW ? " +model.getWhen());
-            how.setText("WHERE ?"+model.getHow());
-            date_time.setText(model.getDate_time());
-
-
+            what.setText("WHAT: " + model.getWhat());
+            when.setText("WHEN: " + model.getWhen());
+            how.setText("HOW: " + model.getHow());
+            date_time.setText("Posted at " + model.getDate_time());
         }
     }
 }
